a1066c8ccd0b2be7d6c54eab1217b32334d777e7
Maksymilian Orzechowski <morzechowski8@gmail.com>
2025-09-11 22:14:50 +0200

    ZipperFile model Refactor

--- app/models/zipper_file.rb
+++ app/models/zipper_file.rb
@@ -15,26 +15,35 @@ class ZipperFile < ApplicationRecord
   def zip_uploaded_file
     return unless unzipped_file.present?
 
-    temp_zip = Tempfile.new(%w[archive_file .zip])
-    temp_zip_path = temp_zip.path
-    original_filename = unzipped_file.original_filename
+    @temp_zip = Tempfile.new(%w[archive_file .zip])
+    @original_filename = unzipped_file.original_filename
+    @random_password = SecureRandom.hex
 
-    random_password = SecureRandom.hex
-    encrypter = Zip::TraditionalEncrypter.new(random_password)
+    encrypt_and_save_zip
+    save_archive_attachment_properties
+    temp_file_cleanup
+  end
 
-    Zip::OutputStream.open(temp_zip_path, encrypter: encrypter) do |output|
-      output.put_next_entry(original_filename)
+  def encrypt_and_save_zip
+    encrypter = Zip::TraditionalEncrypter.new(@random_password)
+
+    Zip::OutputStream.open(@temp_zip.path, encrypter: encrypter) do |output|
+      output.put_next_entry(@original_filename)
       output.write File.read(unzipped_file)
     end
+  end
 
+  def save_archive_attachment_properties
     self.archive_file.attach(
-      io: File.open(temp_zip_path),
-      filename: "#{original_filename}.zip",
+      io: File.open(@temp_zip.path),
+      filename: "#{@original_filename}.zip",
       content_type: "application/zip"
     )
-    self.archive_password = random_password
+    self.archive_password = @random_password
+  end
 
-    temp_zip.close
-    temp_zip.unlink
+  def temp_file_cleanup
+    @temp_zip.close
+    @temp_zip.unlink
   end
 end
